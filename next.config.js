e3a7b50629b3e94c3f33fbe165571453797069a8
sori <hastamanana021110@yahoo.co.jp>
2020-10-12 23:16:25 +0900

    add gtag

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ dotenv.config();
 module.exports = {
   env: {
     GQL_URI: process.env.GQL_URI,
+    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
   },
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
+const gtagScript = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${process.env.GA_TRACKING_ID}');
+`;
+
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: gtagScript,
+            }}
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
