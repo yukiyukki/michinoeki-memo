bc3ffcd963f02d010989921c0f542f4c04bf4a99
sori <hastamanana021110@yahoo.co.jp>
2020-11-28 20:19:56 +0900

    Revert "fix endpoint"
    This reverts commit 1830ae56ff142a0613f1401d1a24d8e66d03d3fd.

--- src/apiClients/prismicClient.ts
+++ src/apiClients/prismicClient.ts
@@ -1,9 +1,7 @@
 import Prismic from 'prismic-javascript';
 
-const {
-  PRISMIC_API_ENDPOINT: apiEndpoint,
-  PRISMIC_ACCESS_TOKEN: accessToken,
-} = process.env;
+const apiEndpoint = process.env.PRISMIC_API_ENDPOINT;
+const accessToken = process.env.PRISMIC_ACCESS_TOKEN;
 
 export const Client = (req = null) =>
   Prismic.client(apiEndpoint, createClientOptions(req, accessToken));
