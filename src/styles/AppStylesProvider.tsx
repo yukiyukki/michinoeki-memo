b2460d95728680d53ef0e4d5b3ecce8cc95b941e
Yuki N <yuki.nagashima@stafes.com>
2022-07-08 23:09:16 +0900

    fix style

--- src/styles/AppStylesProvider.tsx
+++ src/styles/AppStylesProvider.tsx
@@ -7,11 +7,6 @@ import {
 } from '@mui/material/styles';
 import StylesProvider from '@mui/styles/StylesProvider';
 
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
-
 const AppStylesProvider: React.FC = ({ children }) => (
   <StylesProvider injectFirst>
     <StyledEngineProvider injectFirst>
