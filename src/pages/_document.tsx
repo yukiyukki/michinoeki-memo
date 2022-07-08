581e6892c954a897288c48051dc66bea167c9fe1
Yuki N <yuki.nagashima@stafes.com>
2022-07-08 23:30:36 +0900

    set stylesheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 const gtagScript = `
 window.dataLayer = window.dataLayer || [];
@@ -42,3 +43,28 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: (
+        <React.Fragment>
+          {initialProps.styles}
+          {sheet.getStyleElement()}
+        </React.Fragment>
+      ),
+    };
+  } finally {
+    sheet.seal();
+  }
+};
